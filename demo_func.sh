82f3280e316f168da1fbaa04df666af0b7621ae1
Joyce Liao <jycqliao@gmail.com>
2019-03-15 19:17:34 -0400

    fix while loop

--- demo_func.sh
+++ demo_func.sh
@@ -6,7 +6,7 @@ my_func()
 {
 	for i in $@
 	do
-		echo $i
+		`touch "stuff/$i.txt"`
 	done
 
 	g_var="global var has been modified"
@@ -14,9 +14,3 @@ my_func()
 
 my_func come watch SING!
 echo "g_var = $g_var"
-
-echo "What is your name?"
-read USER_NAME
-echo "Hello $USER_NAME"
-echo "I will create you a file called $USER_NAME_file"
-touch "${USER_NAME}_file"
